5d7cc535dbb00b3a3ecafefea16a9d6f5d26d618
caoxuexi <969718359@qq.com>
2021-04-23 14:36:20 +0800

    iot-control

--- pages/iot-control/iot-control.js
+++ pages/iot-control/iot-control.js
@@ -16,8 +16,12 @@ Page({
         if (this.data.status === "开始监控") {
             this.setData({status : "停止监控"})
             this.data.flag=setInterval(this.dealData,3000)
+            console.log(this.data.flag)
         } else if (this.data.status === "停止监控") {
-            clearInterval(this.data.flag)
+            if(this.data.flag){
+                clearInterval(this.data.flag)
+            }
+            console.log(this.data.flag)
             this.setData({status : "开始监控"})
         }
     },
@@ -27,14 +31,16 @@ Page({
         iot_data = await Iot.getTemAndHum();
         console.log(iot_data)
         this.setData({
-            temperature: iot_data.temperature,
+            temperature: iot_data.temperature+" ℃",
             humidity: iot_data.humidity,
         })
     },
 
     //退出页面的时候停止监控
     onUnload() {
-
+        if(this.data.status === "停止监控"){
+            clearInterval(this.data.flag)
+        }
     }
 
 });

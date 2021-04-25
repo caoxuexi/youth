9f8b431291d98640f426eeae1ac65a08f828a45b
caoxuexi <969718359@qq.com>
2021-04-25 14:56:55 +0800

    iot-control

--- pages/iot-control/iot-control.js
+++ pages/iot-control/iot-control.js
@@ -16,12 +16,10 @@ Page({
         if (this.data.status === "开始监控") {
             this.setData({status : "停止监控"})
             this.data.flag=setInterval(this.dealData,3000)
-            console.log(this.data.flag)
         } else if (this.data.status === "停止监控") {
             if(this.data.flag){
                 clearInterval(this.data.flag)
             }
-            console.log(this.data.flag)
             this.setData({status : "开始监控"})
         }
     },
@@ -29,7 +27,6 @@ Page({
     async dealData() {
         let iot_data;
         iot_data = await Iot.getTemAndHum();
-        console.log(iot_data)
         this.setData({
             temperature: iot_data.temperature+" ℃",
             humidity: iot_data.humidity,
@@ -42,5 +39,4 @@ Page({
             clearInterval(this.data.flag)
         }
     }
-
 });
